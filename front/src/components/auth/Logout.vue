f3c522d520e6b544f6d18d19c5647a20535018a8
Ciarán Ainsworth <cda@rootkey.co.uk>
2020-07-16 21:03:30 +0100

    Add support for unauthenticated users hitting the logout page

--- front/src/components/auth/Logout.vue
+++ front/src/components/auth/Logout.vue
@@ -1,12 +1,18 @@
 <template>
   <main class="main pusher" v-title="labels.title">
     <section class="ui vertical stripe segment">
-      <div class="ui small text container">
+      <div v-if="$store.state.auth.authenticated" class="ui small text container">
         <h2>
           <translate translate-context="Content/Login/Title">Are you sure you want to log out?</translate>
         </h2>
         <p v-translate="{username: $store.state.auth.username}" translate-context="Content/Login/Paragraph">You are currently logged in as %{ username }</p>
         <button class="ui button" @click="$store.dispatch('auth/logout')"><translate translate-context="Content/Login/Button.Label">Yes, log me out!</translate></button>
+      </div>
+      <div v-else class="ui small text container">
+        <h2>
+          <translate translate-context="Content/Login/Title">You aren't currently logged in</translate>
+        </h2>
+        <router-link to='/login' class="ui button"><translate translate-context="Content/Login/Button.Label">Log in!</translate></router-link>
       </div>
     </section>
   </main>
